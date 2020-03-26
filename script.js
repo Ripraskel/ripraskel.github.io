b84d6a9fe0a481242145a65eac8c065f387d5449
Richard <riprask@live.com>
2020-03-26 21:26:28 +0000

    Javascript updates and prettyfying the website

--- script.js
+++ script.js
@@ -2,5 +2,31 @@ function sing(song) {
     console.log(song);
 }
 
-sing("hi there!");
+function jump(height) {
+    console.log(height*2);
+}
+
+var hello_song = "Hi hi hi hi"
+sing(hello_song);
 sing("hi there! see you there");
+
+var list = ["tiger", "cat", "bear", "bird"];
+
+sing(list[0]);
+list[0] = "monkey";
+sing(list[0]);
+
+var functionList = [sing, jump];
+functionList[0]("man");
+
+var user = {
+    name : "John",
+    age: 30,
+    isMarried: false,
+};
+
+console.log(user.age);
+
+user.favouriteFood = "Banana";
+
+console.log(user.favouriteFood);
