c23ab7c89eb5c49303e794b68e40cc3718ff37ab
Richard <riprask@live.com>
2020-06-18 12:50:48 +0100

    updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function sing(song) {
     console.log(song);
 }
@@ -30,3 +31,56 @@ console.log(user.age);
 user.favouriteFood = "Banana";
 
 console.log(user.favouriteFood);
+
+// ##############################
+var fruitContainer = document.getElementById("fruitContainer");
+var fruitList = [
+    {
+        name: "tomato",
+        imgSrc: "Images/tomato.jpg"
+    },
+    {
+        name: "cucumbers",
+        imgSrc: "Images/cucumbers.jpg"
+    }
+];
+console.log(fruitList[0].imgSrc);
+
+for (i = 0; i < fruitList.length; i++){
+    var div = document.createElement("div");
+    div.setAttribute("id", fruitList[i].name)
+    div.setAttribute("class", "zone box");
+    var img = document.createElement("img");
+    img.setAttribute("src", fruitList[i].imgSrc);
+    div.appendChild(img);
+    fruitContainer.appendChild(div);
+};
+
+var boxes = document.getElementsByClassName("box");
+var input = document.getElementById("inputbox");
+var cover = document.getElementsByClassName("cover")[0];
+
+function boxClick(event) {
+    console.log(event);
+}
+
+function keypress(event){
+    console.log(event.keyCode);
+}
+
+function toggleCoolStyle(obj) {
+    obj.classList.toggle("coolStyle");
+    console.log(obj.classList);
+}
+
+cover.addEventListener("mouseover", toggleCoolStyle(cover));
+cover.addEventListener("mouseout", toggleCoolStyle(cover));
+
+for (i = 0; i < boxes.length; i++){
+    boxes[i].addEventListener("click", boxClick);
+};
+
+// box.addEventListener("click", boxClick);
+// box.addEventListener("mouseover", boxClick);
+
+input.addEventListener("keypress", keypress);
